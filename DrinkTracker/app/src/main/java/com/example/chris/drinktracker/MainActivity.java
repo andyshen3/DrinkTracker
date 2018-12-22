c6e9e662b5c105bbab2560536b0faef621d265f4
cchrism0426 <cchrism0426@gmail.com>
2018-12-22 11:30:29 -0800

    added settings page

--- DrinkTracker/app/src/main/java/com/example/chris/drinktracker/MainActivity.java
+++ DrinkTracker/app/src/main/java/com/example/chris/drinktracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.chris.drinktracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToSettings(View view) {
+        Intent intent = new Intent(this, Settings.class);
+        startActivity(intent);
     }
 
     public void sendMessage(View view)
