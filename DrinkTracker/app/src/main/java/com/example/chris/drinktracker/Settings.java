df881f20654914232f5302baff118c8ab6ef005a
Andy <andyshen2882@gmail.com>
2018-12-23 21:25:53 -0800

    Link Settings back to home screen

--- DrinkTracker/app/src/main/java/com/example/chris/drinktracker/Settings.java
+++ DrinkTracker/app/src/main/java/com/example/chris/drinktracker/Settings.java
@@ -1,7 +1,10 @@
 package com.example.chris.drinktracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button confirmButton = (Button) findViewById(R.id.confirmButton);
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view)
+            {
+                Intent intent = new Intent(Settings.this, HomePage.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
