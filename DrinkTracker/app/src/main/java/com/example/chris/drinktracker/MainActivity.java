95914a2004c905dd53a4ad2942e4872ccea6316f
cchrism0426 <cchrism0426@gmail.com>
2018-12-24 12:13:45 -0800

    implemented shared preferences for sign in

--- DrinkTracker/app/src/main/java/com/example/chris/drinktracker/MainActivity.java
+++ DrinkTracker/app/src/main/java/com/example/chris/drinktracker/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.chris.drinktracker;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +17,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void goToHomePage(View view) {
+    public void hitConfirm(View view) {
+        EditText username = (EditText) findViewById(R.id.userName);
+        EditText weight = (EditText) findViewById(R.id.weightInput);
+        String usernameValue = username.getText().toString();
+        if (usernameValue.equals("")) {
+            usernameValue = "Bob Joe Shen";
+        }
+        Float weightValue = 0f;
+        if (weight.getText().toString().equals("")) {
+            weightValue = 420f;
+        } else {
+            weightValue = Float.valueOf(weight.getText().toString());
+        }
+        RadioGroup genderGroup = (RadioGroup) findViewById(R.id.genderSelect);
+        RadioButton genderButton = (RadioButton) findViewById(genderGroup.getCheckedRadioButtonId());
+        SharedPreferences.Editor editor = getPreferences(MODE_PRIVATE).edit();
+        editor.putString("username", usernameValue);
+        editor.putString("gender", genderButton.getText().toString());
+        editor.putFloat("weight", weightValue);
         Intent intent = new Intent(this, HomePage.class);
         startActivity(intent);
     }
--- DrinkTracker/app/src/main/java/com/example/chris/drinktracker/Settings.java
+++ DrinkTracker/app/src/main/java/com/example/chris/drinktracker/Settings.java
@@ -1,6 +1,7 @@
 package com.example.chris.drinktracker;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,6 +13,10 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        String username = prefs.getString("username", null);
+        String gender = prefs.getString("gender", null);
+        Float weight = prefs.getFloat("weight", 0f);
 
         Button confirmButton = (Button) findViewById(R.id.confirmButton);
         confirmButton.setOnClickListener(new View.OnClickListener() {
