f6608872181525d126a576dea350a18ab97ba022
Andy <andyshen2882@gmail.com>
2018-12-28 15:42:15 -0800

    Added settings and add button assets

--- DrinkTracker/app/src/main/java/com/example/chris/drinktracker/HomePage.java
+++ DrinkTracker/app/src/main/java/com/example/chris/drinktracker/HomePage.java
@@ -2,10 +2,13 @@ package com.example.chris.drinktracker;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
@@ -18,6 +21,14 @@ public class HomePage extends AppCompatActivity {
         String text = prefs.getString("username", "No Name");
         TextView name = (TextView) findViewById(R.id.homePageName);
         name.setText(text);
+
+        Resources res = getResources();
+        Drawable drawable = res.getDrawable(R.drawable.circle);
+        final ProgressBar progBar = (ProgressBar) findViewById(R.id.circularProgressbar);
+        progBar.setProgress(0);
+        //Set Max as the max BAC for the user, default to 100 for now
+        progBar.setMax(100);
+        progBar.setProgressDrawable(drawable);
 
     }
 
